605959ff8af588148a979fa13075d3ffcafac3fe
Vyankatkar Manmath <vyankatkarmanmath45@gmail.com>
2026-04-25 20:16:08 +0530

    Add Addtion program

---
+++ Addition.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0; 
+    int j = 0;
+    int Ans = 0;
+
+    printf("Enter i : \n");
+    scanf("%d",&i);
+
+    printf("Enter j : \n");
+    scanf("%d",&j);
+
+    Ans = i+j;
+
+    printf("Addition is : %d\n",Ans);
+
+    return 0;
+}
